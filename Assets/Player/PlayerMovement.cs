a1ef5ddd9adbb5b5f8ec9c83c9b35e330e06906f
TreeStew <tristanfitzwater@gmail.com>
2017-09-23 12:10:02 -0400

    movementUpdate
    addedMovement

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Rigidbody rb;
+    public float speed;
+    public string hAxis;
+    public string vAxis;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float hSpeed = Input.GetAxis(hAxis);
+        float vSpeed = Input.GetAxis(vAxis);
+        rb.AddForce(new Vector3(hSpeed, 0, vSpeed) * speed);
+    }
 }
