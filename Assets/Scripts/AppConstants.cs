057585cfa254791bee8addd22f177e24a6a6950c
moonbuffalo <gnick96@gmail.com>
2017-09-27 20:40:30 -0400

    updated values

--- Assets/Scripts/AppConstants.cs
+++ Assets/Scripts/AppConstants.cs
@@ -9,11 +9,8 @@ using System.Text;
         public static int numRounds = 5;
         public static int currentRound = 0;
         public static float shoutRefreshTime = 2.0f;
-    public static int[] score;
-
+        public static int[] score;
         public static int[] playerControls = new int[] {1,2,3,4};
         public static bool[] playerInMatch = new bool[] { false, false, false, false };
-
-
 }
 
